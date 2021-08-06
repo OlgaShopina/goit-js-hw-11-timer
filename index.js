f259611afb42052aa1fc8ffb62fc35aa4c1248c3
OlgaShopina <olga.shopina@gmail.com>
2021-08-06 21:24:11 +0300

    Таймер

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
+class CountdownTimer {
+    constructor({ selector, targetDate }) {
+        this.selector = selector;
+        this.targetDate = new Date(targetDate);      
+        this.refs = {
+            daysSpan: document.querySelector('span[data-value="days"]'),
+            hoursSpan: document.querySelector('span[data-value="hours"]'),
+            minsSpan: document.querySelector('span[data-value="mins"]'),
+            secsSpan: document.querySelector('span[data-value="secs"]'),
+        };
+    }
 
+   update() {
+    const currentTime = new Date();
+    this.action(currentTime);
+  }
 
+    action(currentTime) {
+    const time = this.targetDate - currentTime;
+    this.refs.daysSpan.textContent = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    this.refs.hoursSpan.textContent = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    this.refs.minsSpan.textContent = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    this.refs.secsSpan.textContent = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    }
 
-new CountdownTimer({
-    selector: '#timer-1',
-    targetDate: new Date('Jul 17, 2019'),
+    pad(value) {
+        return value < 10 ? `0${value}` : value;
+    }
+
+    start() { 
+        setInterval(() =>
+            this.update(),
+    1000)}
+}
+
+let timer01 = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Sept 10, 2021'),
 });
+
+timer01.start();
